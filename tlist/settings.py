c9b2b1593c85a85a3076061ec870c5ff23afb1af
Ravi Kumar <ivardu19@gmail.com>
2020-05-30 22:07:55 +0530

    settings with AWS updated

--- tlist/settings.py
+++ tlist/settings.py
@@ -130,9 +130,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+
+
+AWS_ACCESS_KEY_ID=os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_STORAGE_BUCKET_NAME=os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_SECRET_ACCESS_KEY=os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_DEFAULT_ACL=None
 
 
