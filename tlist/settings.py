66580b532716a89f4ad6701b1149451b174c16f1
Ravi Kumar <ivardu19@gmail.com>
2020-05-31 17:19:17 +0530

    changed

--- tlist/settings.py
+++ tlist/settings.py
@@ -140,7 +140,7 @@ AWS_DEFAULT_ACL=None
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-# settings for s3 storage
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
