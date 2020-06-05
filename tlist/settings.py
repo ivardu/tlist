f1bf0a31831572a16f95162404a5f1ab6edc8074
Ravi Kumar <ivardu19@gmail.com>
2020-06-05 21:58:51 +0100

    changes to settings

--- tlist/settings.py
+++ tlist/settings.py
@@ -199,4 +199,4 @@ AWS_DEFAULT_ACL=None
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
